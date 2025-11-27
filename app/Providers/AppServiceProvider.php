b2e9ac656552a0b6f164bf3335f6c153f57b5351
Aes Saputra <mail@aessaputra.net>
2025-11-27 22:24:22 +0700

    feat: Add conditional Debugbar enabling/disabling based on IP and database seeding for parallel testing.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,11 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Skip debugbar configuration if package is not installed.
-        // if (! class_exists(Debugbar::class)) {
-        //     return;
-        // }
-
         $allowedIPs = array_map('trim', explode(',', config('app.debug_allowed_ips')));
 
         $allowedIPs = array_filter($allowedIPs);
